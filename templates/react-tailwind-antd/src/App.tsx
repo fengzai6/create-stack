9086ef7654e1e74c60e16fc1a811b9c110e28d1b
Nacho.L <112751823+fengzai6@users.noreply.github.com>
2026-03-13 10:42:33 +0800

    Update vite to 8.0

--- templates/react-tailwind-antd/src/App.tsx
+++ templates/react-tailwind-antd/src/App.tsx
@@ -1,7 +1,7 @@
 import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider } from "antd";
+import reactLogo from "@/assets/react.svg";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
